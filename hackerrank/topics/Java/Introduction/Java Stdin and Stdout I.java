e959a36be3bbe81b149e858bdfd4cb5ffcca4771
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2023-05-30 21:37:04 -0300

    Update Java Stdin and Stdout I.java

--- hackerrank/topics/Java/Introduction/Java Stdin and Stdout I.java
+++ hackerrank/topics/Java/Introduction/Java Stdin and Stdout I.java
@@ -1,14 +1,25 @@
-package com.java.hackerrank.dashboard.basics;
-
-import java.util.Scanner;
-
 /**
  * Task
  * 
- * In this challenge, you must read integers from stdin and then print them to
- * stdout. Each integer must be printed on a new line. To make the problem a
- * little easier, a portion of the code is provided for you in the editor below.
+ * In this challenge, you must read  integers from stdin and then print them to stdout. 
+ * Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided 
+ * for you in the editor below.
+ *
+ * Input Format
+ * There are  lines of input, and each line contains a single integer.
+ *
+ * Sample Input
+ * 42
+ * 100
+ * 125
+ * Sample Output
+ * 42
+ * 100
+ * 125
  **/
+
+import java.util.Scanner;
+
 public class StdinAndStdout {
 
 	public static void main(String[] args) {
