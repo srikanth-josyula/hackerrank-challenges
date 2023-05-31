36661b86919b469a8538f9cd6b310e4ef6ba68f2
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2023-05-30 21:52:02 -0300

    Update Weather Observation Station 3.sql

--- hackerrank/topics/SQL/Weather Observation Station 3.sql
+++ hackerrank/topics/SQL/Weather Observation Station 3.sql
@@ -1,4 +1,4 @@
-Query a list of CITY names from STATION for cities that have an even ID number. 
+/*Query a list of CITY names from STATION for cities that have an even ID number. 
 Print the results in any order, but exclude duplicates from the answer. 
 The STATION table is described as follows:
 
@@ -11,7 +11,7 @@ The STATION table is described as follows:
 | LAT N  | NUMBER       | YES  |     | NULL    |       |
 | LONG W | NUMBER       | YES  |     | NULL    |       |
 +--------+--------------+------+-----+---------+-------+
-
+*/
 
 ANSWER:
 SELECT DISTINCT CITY
