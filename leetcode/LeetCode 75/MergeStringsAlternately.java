b7525af2045853b9f16cf53441987f39f7e6ccaf
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2024-12-04 14:39:36 -0400

    Update MergeStringsAlternately.java

--- leetcode/LeetCode 75/MergeStringsAlternately.java
+++ leetcode/LeetCode 75/MergeStringsAlternately.java
@@ -6,6 +6,7 @@ public class Solution {
 		System.out.println(mergeAlternately(word1, word2));
 	}
 
+	//This took 100ms
 	public static String mergeAlternately(String word1, String word2) {
 
 		String output = "";
@@ -32,4 +33,33 @@ public class Solution {
 		}
 		return output;
 	}
+
+//same code with stringbuilder took 0ms
+    public String mergeAlternately(String word1, String word2) {
+      
+		StringBuilder output = new StringBuilder();
+
+		char[] ch1 = word1.toCharArray();
+		char[] ch2 = word2.toCharArray();
+
+		int limit = 0;
+		if (ch1.length > ch2.length) {
+			limit = ch1.length;
+		} else {
+			limit = ch2.length;
+		}
+
+		for (int i = 0; i < limit; i++) {
+
+			if (i < ch1.length) {
+				output.append(ch1[i]);
+			}
+
+			if (i < ch2.length) {
+				output.append(ch2[i]);
+			}
+		}
+		return output.toString();
+	}
+
 }
