d1b5d93810ab7a4da8729a83db1547d3b711f992
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2024-12-06 15:59:50 -0400

    Update RomantoInteger.java

--- leetcode/Top Interview 150/RomantoInteger.java
+++ leetcode/Top Interview 150/RomantoInteger.java
@@ -14,25 +14,46 @@ public class Solution {
 
 	public static int romanToInt(String s) {
 
-		Map<Character, Integer> m = new HashMap<>();
+		/*Map<Character, Integer> m = new HashMap<>();
 		m.put('I', 1);
 		m.put('V', 5);
 		m.put('X', 10);
 		m.put('L', 50);
 		m.put('C', 100);
 		m.put('D', 500);
-		m.put('M', 1000);
+		m.put('M', 1000);*/
+		
+		int[] m = new int[256];  // Array size 256 to cover all possible ASCII characters
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
 
 		int ans = 0;
 
-		for (int i = s.length() - 1; i >= 0; i--) {
+		//Using Map ran in 6ms
+		/*for (int i = s.length() - 1; i >= 0; i--) {
 			if (i > 0 && m.get(s.charAt(i - 1)) < m.get(s.charAt(i))) {
 				ans += (m.get(s.charAt(i)) - m.get(s.charAt(i - 1)));
 				i--;
 			} else {
 				ans += (m.get(s.charAt(i)));
 			}
+		}*/
+		
+		//Ran in 3ms
+		for (int i = s.length() - 1; i >= 0; i--) {
+			if (i > 0 && m[s.charAt(i - 1)] < m[s.charAt(i)]) {
+				ans += (m[s.charAt(i)] - m[s.charAt(i - 1)]);
+				i--;
+			} else {
+				ans += (m[s.charAt(i)]);
+			}
 		}
+		
 		return ans;
 	}
 }
