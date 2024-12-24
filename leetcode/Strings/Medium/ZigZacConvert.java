31707a0b5152ce3e0ea806c023a7a219c188ec0f
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2024-12-24 10:23:05 -0500

    Update ZigZacConvert.java

--- leetcode/Strings/Medium/ZigZacConvert.java
+++ leetcode/Strings/Medium/ZigZacConvert.java
@@ -12,9 +12,9 @@ public class Solution {
 
 	public static String convert(String s, int numRows) {
 	
-		//[00]P       [04]A	      [08]H       [12]N	
-		//[01]A	[03]P [05]L	[07]S [09]I [11]I [13]G
-		//[02]Y       [06]I       [10]R  
+		//[00]P       [04]A	   [08]H       [12]N	
+		//[01]A	[03]P [05]L  [07]S [09]I [11]I [13]G
+		//[02]Y       [06]I        [10]R  
 	
 		//For i = 0 => j = 0, 4, 8, 12             => Incrementing by 4  => j + ((2*numRows)-2) * i
 		//For i = 1 => j = 1, 3, 5,  7, 09, 11, 13 => Incrementing by 2  => j + ((2*numRows)-2) * i
