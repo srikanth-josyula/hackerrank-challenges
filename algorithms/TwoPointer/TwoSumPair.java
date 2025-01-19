c33536655b0a993fd15038a1d10b2cec0dd3af36
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2025-01-19 14:43:15 -0400

    Create TwoSumPair.java

---
+++ algorithms/TwoPointer/TwoSumPair.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class TwoSum {
+    public static int[] findTwoSum(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            
+            if (sum == target) {
+                return new int[]{nums[left], nums[right]};
+            } else if (sum < target) {
+                left++; // Move left pointer to increase sum
+            } else {
+                right--; // Move right pointer to decrease sum
+            }
+        }
+        
+        return new int[0]; // Return an empty array if no pair is found
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 6};
+        int target = 6;
+        System.out.println(Arrays.toString(findTwoSum(nums, target))); // Output: [2, 4]
+    }
+}
